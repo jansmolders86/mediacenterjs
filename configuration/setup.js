fba10df8c330ebe377894f7b24d64ad4611f51b8
Jan Smolders <jansmolders86@gmail.com>
2013-05-07 13:51:04 +0200

    Reverted to default values

--- configuration/setup.js
+++ configuration/setup.js
@@ -1,11 +1,11 @@
 {
-    "moviepath": "C:/temp/",
+    "moviepath": "",
     "highres": "yes",
     "musicpath": "tbd",
     "tvpath": "tbd",
-    "language": "nl",
+    "language": "",
     "onscreenkeyboard": "no",
-    "location": "eindhoven",
+    "location": "",
     "screensaver": "dim",
     "port": "3000"
 }
