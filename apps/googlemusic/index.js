877cc4c00c5db83d3fc4780414c42a73e2e03cdb
Jan Smolders <jansmolders86@gmail.com>
2013-05-15 13:33:09 +0200

    Added placeholder

--- apps/googlemusic/index.js
+++ apps/googlemusic/index.js
@@ -1,24 +1,4 @@
 /*
-	MediaCenterJS - A NodeJS based mediacenter solution
-	
-    Copyright (C) 2013 - Jan Smolders
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
-// Choose your render engine. The default choice is JADE:  http://jade-lang.com/
-exports.engine = 'jade';
 var GoogleMusic = require('googlemusicapi').GoogleMusicApi;
 var googlemusic = new GoogleMusic('jansmolders86@gmail.com', '');
 // Render the indexpage
@@ -43,3 +23,29 @@ exports.index = function(req, res, next){
 	
 };
 */
+
+/*
+	MediaCenterJS - A NodeJS based mediacenter solution
+	
+    Copyright (C) 2013 - Jan Smolders
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+// Choose your render engine. The default choice is JADE:  http://jade-lang.com/
+exports.engine = 'jade';
+
+exports.index = function(req, res, next){
+	res.render('gmusic');
+};
