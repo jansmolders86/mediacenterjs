88b2b91615ff7c18436d88cefbf353f4bb577d66
Jan Smolders <jansmolders86@gmail.com>
2013-05-31 20:10:31 +0200

    New module FS Extra
    Added new module

---
+++ node_modules/fs.extra/fs.copy.js
@@ -0,0 +1,43 @@
+(function () {
+  "use strict";
+
+  var fs = require('fs')
+    ;
+
+  function noop() {}
+
+  function copy(src, dst, cb) {
+    function copyHelper(err) {
+      var is
+        , os
+        ;
+
+      if (!err) {
+        return cb(new Error("File " + dst + " exists."));
+      }
+
+      fs.stat(src, function (err, stat) {
+        if (err) {
+          return cb(err);
+        }
+
+        is = fs.createReadStream(src);
+        os = fs.createWriteStream(dst);
+
+        is.pipe(os);
+        os.on('close', function (err) {
+          if (err) {
+            return cb(err);
+          }
+
+          fs.utimes(dst, stat.atime, stat.mtime, cb);
+        });
+      });
+    }
+
+    cb = cb || noop;
+    fs.stat(dst, copyHelper);
+  }
+
+  module.exports = copy;
+}());
