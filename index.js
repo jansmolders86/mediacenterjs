3acf5b6662eff743b002d35e240b88fef463fd6b
Jan Smolders <jan.smolders@isaac.nl>
2013-07-10 13:04:20 +0200

    Removed Geoip module

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ var express = require('express')
 , fs = require ('fs')
 , dateFormat = require('dateformat')
 , lingua = require('lingua')
-, geoip = require('geoip-lite')
 , colors = require('colors')
 , rimraf = require('rimraf');
 
@@ -72,16 +71,7 @@ app.configure('production', function(){
 require('./lib/routing')(app,{ verbose: !module.parent });
 app.get("/", function(req, res, next) {  
 	if(	configfileResults.moviepath == '' && configfileResults.language == '' && configfileResults.location == '' || configfileResults.moviepath == null || configfileResults.moviepath == undefined){
-		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-		, locationFound = 'unknown';
-		
-		if( ip !== '127.0.0.1'){
-			var geo = geoip.lookup(ip);
-			if(geo !== null) locationFound = geo.city
-		} 
-		res.render('setup',{
-			location: locationFound
-		});	
+		res.render('setup');	
 	} else {
 		var apps = []
 		//Search app folder for apps and check if tile icon is present
