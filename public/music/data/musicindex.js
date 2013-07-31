8ec86dfd14bf2fa2e9da458dc010505fa3040538
Jan Smolders <jan.smolders@isaac.nl>
2013-07-31 13:53:34 +0200

    Lame updated

--- public/music/data/musicindex.js
+++ public/music/data/musicindex.js
@@ -1,3 +1,12 @@
 [
-
+    "Anamanaguchi - Power Supply",
+    "Depeche Mode - Violator",
+    "Disclosure - Settle",
+    "Queens Of The Stone Age ...Like Clockwork",
+    "Rudimental - Home",
+    "Skrillex - BBC Radio 1 Essential Mix.mp3",
+    "Sound City - Real to Reel",
+    "Stone Sour - House Of Gold & Bones Part 2",
+    "Tenacious D - Tenacious D",
+    "The Strokes - Comedown Machine"
 ]
