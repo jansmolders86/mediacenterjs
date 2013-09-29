e21f4695a756ab2d8a2edf0984a22eaecdffb9ad
jansmolders86 <jansmolders86@gmail.com>
2013-09-29 11:59:19 +0200

    Removed test data

--- public/movies/data/movieindex.js
+++ public/movies/data/movieindex.js
@@ -1,10 +1,2 @@
 [
-    "avatar.avi",
-    "despicable me 2 CD1 2013.avi",
-    "despicable me 2 CD2.avi",
-    "pi.avi",
-    "sdfdfdf.avi",
-    "the avengers.avi",
-    "up.avi",
-    "wall-e.avi"
 ]
