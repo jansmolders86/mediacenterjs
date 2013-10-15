1f9cfdb9771189d3e6fa1c8b5cebc5f563ec7afd
Jan Smolders <jan.smolders@isaac.nl>
2013-10-15 15:29:23 +0200

    WIP Basic setup for plugin manager

---
+++ apps/plugins/plugins-functions.js
@@ -0,0 +1,129 @@
+var express = require('express')
+	, app = express()
+	, fs = require('fs')
+	, ini = require('ini')
+	, config = ini.parse(fs.readFileSync('./configuration/config.ini', 'utf-8'))
+	, exec = require('child_process').exec
+	, async = require('async')
+	, pluginPrefix = config.pluginPrefix
+	, npm = 'npm'
+	, install = npm + ' install '
+	, remove = npm + ' remove '
+	, plugins = []
+	, installedPlugins = [];
+
+exports.getAvailablePlugins = function(req, res){
+
+	var getInstalledPlugins = function(){
+
+		var nodeModules = __dirname + '/../../node_modules';
+
+		fs.readdirSync(nodeModules).forEach(function(name){
+
+			//Check if the folder in the node_modules starts with the prefix
+			if(name.substr(0, pluginPrefix.length) !== pluginPrefix)
+				return;
+
+			installedPlugins.push(name);
+
+		});
+	};
+
+	console.log('getAvailablePlugins')
+	var npm = 'npm'
+		, search = npm + ' search '
+		, pluginPrefix = config.pluginPrefix
+		, plugins = []
+
+	exec(search + pluginPrefix, function callback(error, stdout, stderr){
+		//TODO: NEED TO CACHE THE SEARCH RESULTS!!! SLOWWWWWW Page loads.
+		if (error){
+			console.log('Error: Unable to retieve plugins list');
+		}
+		var plugins = buildPluginList(stdout);
+		console.log(plugins);
+	});
+
+	var buildPluginList = function(stdout){
+		var list = stdout.split('\n');
+		var plugins = [];
+		list.forEach(function(p, i){
+			//First removes header, 
+			//second removes blank lastline.
+			//third removes npm updates
+			if (p.substr(0, 4) === 'NAME' || i === list.length - 1 || p.substr(0,3) === 'npm') return;  
+			var s = p.split(' ');
+			var name = s[0];
+
+			p = p.replace(name, '');
+			s = p.split('=');
+			var desc = s[0];
+			p = p.replace(desc, '');
+			s = p.split(' ');
+
+			var plugin = {
+				name: name.replace(pluginPrefix, ''), //Remove the Mediacenterjs-
+				desc: desc,
+				author: s[0].substr(1),
+				date: s[2] + ' ' + s[3],
+				version: s[5],
+				isInstalled: contains(installedPlugins, name)
+			}
+
+			plugins.push(plugin);
+		});
+
+		return plugins;
+	};
+	
+	var contains = function(array, value){
+		if (array instanceof Array === false)
+			return false;
+
+		var isFound = false;
+		array.forEach(function(val){
+			if (val === value) {
+				isFound = true;
+				return false; //break loop;
+			}
+		});
+		return isFound;
+	};
+
+};	
+
+exports.uninstallPlugin = function(req, res, pluginName){
+	console.log('Plugins.uninstallPlugin');
+	
+	if (!plugin || plugin === undefined)
+		return;
+
+	var name = pluginPrefix + pluginName;
+	console.log('Plugins.uninstallPlugin: Uninstalling ' + name);
+
+	exec(remove + name, function callback(error, stdout, stderr){
+		if (error){
+			console.log("Error: Unable to uninstall plugin: " + name);
+			return;
+		}			
+		console.log('Plugins.uninstallPlugin: Uninstalled');			
+	});
+};
+
+
+exports.installPlugin = function(req, res, pluginName){
+	console.log('Plugins.installPlugin');
+
+	if (!plugin || plugin === undefined)
+		return;
+
+	var name = pluginPrefix + pluginName;
+	console.log('Plugins.installPlugin: Installing ' + name);
+	exec(install + name, function callback(error, stdout, stderr){
+		if (error){
+			console.log("Error: Unable to install plugin: " + name);
+			return;
+		}			
+		console.log('Plugins.installPlugin: installed');			
+	});
+};
