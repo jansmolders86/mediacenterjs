6c24835139a59b041a16b71b2dba3298a07a4a80
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-10-20 02:32:06 -0200

    Portuguese translation files

---
+++ public/translations/frontend/translation_pt.js
@@ -0,0 +1,11 @@
+succesMessage = Executado com sucesso.
+weather_cloudy = nublado
+weather_sunny = ensolarado
+weather_rain = chuvoso
+weather_mist = neblina
+weather_clear = claro
+weather_snow = neve
+weather_storm = tempestado
+feelsLike = Feels like
+error_weather =  Especifique uma cidade maior
+confirmMessage = Tem certeza que deseja fazer isso? Essa ação não poderá ser revertida.
