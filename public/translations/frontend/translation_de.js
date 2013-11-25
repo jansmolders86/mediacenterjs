878edaa114b7d4b149aac7d5d4eafb1753b14b6e
hoffi <stefhoff@go4more.de>
2013-11-25 22:19:49 +0100

    Added german translation

---
+++ public/translations/frontend/translation_de.js
@@ -0,0 +1,11 @@
+succesMessage = Erfolgreich ausgeführt.
+weather_cloudy = Bedeckt
+weather_sunny = Sonnig
+weather_rain = Regen
+weather_mist = Nebel
+weather_clear = Klar
+weather_snow = Schnee
+weather_storm = Sturm
+feelsLike = Gefühlte
+error_weather =  Bitte geben Sie eine größere Stadt an.
+confirmMessage = Sind Sie sicher, dass die das tun möchten? Diese Aktion kann nicht rückgängig gemacht werden.
