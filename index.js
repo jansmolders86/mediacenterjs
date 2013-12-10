5a8392413c829181c6ffcfae3882fe9c24140509
Jan Smolders <jan.smolders@isaac.nl>
2013-12-10 19:13:29 +0100

    Fix for Connect alert (replaced bodyParser)

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ app.configure(function(){
 	app.set('view engine', 'jade');
 	app.set('views', __dirname + '/views');
 	app.setMaxListeners(100);
-	app.use(express.bodyParser());
+    app.use(express.json());
+    app.use(express.urlencoded());
 	app.use(express.methodOverride());
 	app.use(express.static(__dirname + '/public'));
 	app.use(express.favicon(__dirname + '/public/core/favicon.ico'));
