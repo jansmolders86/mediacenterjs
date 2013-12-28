e592d1f1b22ff21eab5dce3e1683595455edf6c0
jansmolders86 <jansmolders86@gmail.com>
2013-12-28 19:20:31 +0100

    Code cleanup, fixed getting duration

--- apps/movies/movie-playback-handler.js
+++ apps/movies/movie-playback-handler.js
@@ -3,9 +3,7 @@ var colors = require('colors')
 	, ffmpeg = require('fluent-ffmpeg')
 	, os = require('os')
 	, fs = require('fs.extra')
-	, config = require('../../lib/handlers/configuration-handler').getConfiguration()
-	, probe = require('node-ffprobe')
-	, dblite = require('dblite');
+	, config = require('../../lib/handlers/configuration-handler').getConfiguration();
 
 /* Public Methods */
 
@@ -27,43 +25,12 @@ exports.startPlayback = function(response, movieUrl, movieFile, platform) {
 
 	if(fs.existsSync(outputPath) === true){
 		fs.unlinkSync(outputPath);
-	};       
-
-	probe(movieUrl, function(err, probeData) {
-		if(probeData.streams[0].duration !== 0 || probeData.streams[0].duration !== undefined || probeData.streams[0].duration !== 'N/A' ){
-			var data = { 
-				'platform': platform,
-				'duration':probeData.streams[0].duration
-			}
-		} else {
-			if(os.platform() === 'win32'){
-				dblite.bin = "./bin/sqlite3/sqlite3";
-			}
-			var db = dblite('./lib/database/mcjs.sqlite');
-			db.on('info', function (text) { console.log(text) });
-			db.on('error', function (err) { console.error('Database error: ' + err) });
-			db.query('SELECT * FROM movies WHERE local_name =? ', [ movieRequest ], {
-					runtime : String,
-				},
-				function(rows) {
-					if (typeof rows !== 'undefined' && rows.length > 0){
-						var data = { 
-							'platform': platform,
-							'duration': rows.runtime
-						}
-						response.json(data);  
-					} else {
-						console.log('Unknow movie duration, falling back to estimated duration.' .red);
-						var data = { 
-							'platform': platform,
-							'duration': 9000
-						}
-						response.json(data);  
-					}
-				}
-			);
-		}
-			
+	};   
+	    
+	GetMovieDurarion(response, movieUrl, movieFile, platform, function(data){
+		
+		response.json(data);  
+		
 		switch (platform) {
 			case "browser":
 				var ffmpeg = 'ffmpeg -i "'+movieUrl+'" -g 52 -threads 0 -vcodec libx264 -coder 0 -flags -loop -pix_fmt yuv420p -crf 22 -subq 0 -preset ultraFast -acodec copy -sc_threshold 0 -movflags +frag_keyframe+empty_moov '+outputPath
@@ -81,16 +48,65 @@ exports.startPlayback = function(response, movieUrl, movieFile, platform) {
 		
 		
 		var exec = require('child_process').exec
-		, child = exec(ffmpeg, ExecConfig, function(err, stdout, stderr) {
-			if (err) {
-				console.log('FFMPEG error: ',err) ;
-			} else{
-				console.log('Transcoding complete');
-			}
-		});
+			, child = exec(ffmpeg, ExecConfig, function(err, stdout, stderr) {
+				if (err) {
+					console.log('FFMPEG error: ',err) ;
+				} else{
+					console.log('Transcoding complete');
+				}
+			});
 
 		child.stdout.on('data', function(data) { console.log(data.toString()); });
 		child.stderr.on('data', function(data) { console.log(data.toString()); });			
+	});
+};
+
+/* Private Methods */
+
+GetMovieDurarion = function(response, movieUrl, movieFile, platform, callback) {
+	var probe = require('node-ffprobe')
+	, dblite = require('dblite');
+	
+	console.log('Probing file for metadata');
+	
+	probe(movieUrl, function(err, probeData) {
+		if(probeData.streams[0].duration !== 0 && probeData.streams[0].duration !== undefined && probeData.streams[0].duration !== "N/A" ){
+			console.log('Metadata found, continuing...');
+			var data = { 
+				'platform': platform,
+				'duration':probeData.streams[0].duration
+			}
+		   callback(data);
+		} else {
+			if(os.platform() === 'win32'){
+				dblite.bin = "./bin/sqlite3/sqlite3";
+			}
+			var db = dblite('./lib/database/mcjs.sqlite');
+			db.on('info', function (text) { console.log(text) });
+			db.on('error', function (err) { console.error('Database error: ' + err) });
+			db.query('SELECT * FROM movies WHERE local_name =? ', [ movieFile ], {
+					runtime : String,
+				},
+				function(rows) {
+					if (typeof rows !== 'undefined' && rows.length > 0){
+						console.log('Falling back to IMDB runtime information',rows.runtime .blue);
+						var data = { 
+							'platform': platform,
+							'duration': rows.runtime
+						}
+						callback(data);
+					} else {
+						console.log('Unknown movie duration, falling back to estimated duration.' .red);
+						var data = { 
+							'platform': platform,
+							'duration': 9000
+						}
+						callback(data);
+					}
+				}
+			);
+		}
 
 	});
+
 };
