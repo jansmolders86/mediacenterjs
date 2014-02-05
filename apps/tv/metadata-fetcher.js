c3f82d83975fad5b5f9a1154575185971fa094e1
Jan Smolders <jan.smolders@isaac.nl>
2014-02-05 18:50:22 +0100

    WIP: Getting TV app to work
    TV currently breaks but i'm working on it!

--- apps/tv/metadata-fetcher.js
+++ apps/tv/metadata-fetcher.js
@@ -2,7 +2,7 @@
 var fs = require('fs'),
 	os = require('os'),
 	path = require('path'),
-	app_cache_handler = require('../../lib/handlers/app-cache-handler'),
+    app_cache_handler = require('../../lib/handlers/app-cache-handler'),
 	config = require('../../lib/handlers/configuration-handler').getConfiguration(),
 	tv_title_cleaner = require('../../lib/utils/title-cleaner');
 
@@ -26,65 +26,108 @@ db.on('error', function (err) { console.error('Database error: ' + err) });
 exports.fetchMetadataForTvShow = function(tvShow, callback) {
 	var originalTitle = tvShow;
 	var tvShowInfos = tv_title_cleaner.cleanupTitle(tvShow);
-   var tvTitle = tvShowInfos.title;
+    var tvTitle = tvShowInfos.title;
 
-	loadMetadataFromDatabase(tvShow, function (result) {
-		if (result) {
-			callback(result);
-			return;
-		}
+    getEpisodeData( tvTitle, function(episodedata) {
+        var showTitle = episodedata.tvShowTitle;
 
-		fetchMetadataFromTrakt(tvTitle, function(err, result) {
-			var banneImage = "/tv/css/img/nodata.jpg";
-			var title = tvTitle;
-			var genre = "No data";
-			var certification = "No data";
-			
-			if (err) {
-				console.error(err);
-				callback([]);
-			} else {
-				var traktResult = result;
-				app_cache_handler.ensureCacheDirExists('tv', tvTitle);
-				var banner	= traktResult.images.banner;
-				var banneImage = '/data/tv/'+tvTitle+'/'+path.basename(banner);
-			}
+        loadShowMetadataFromDatabase(showTitle, function (result) {
+            if (result) {
+                console.log('tvShow found in database', result);
+                callback(result);
+                return;
+            }
 
-			downloadTvShowBanner(banner, tvTitle, function(err) {
+            console.log('New tvShow, getting data!');
+            loadEpisodeMetadataFromDatabase(tvShow, function (result) {
+                if (result) {
+                    console.log('tvEpisode found in database', result);
 
-				if (err) {
-					console.error(err);
-				} 
-				
-            if(traktResult !== null){  
-                if(traktResult.genre !== undefined){
-             		genre = traktResult.genre;
+                    getMetadataForShow(tvShow, function(showMetaData){
+                        console.log('Found show metadata', showMetaData);
+                        storeShowMetadataInDatabase(showMetaData, function() {
+                            console.log('Show data stored in database!', showMetaData);
+                            var tvShowtitle = showMetaData.title;
+                            loadShowMetadataFromDatabase(tvShowtitle, callback);
+                        });
+                    });
+                } else {
+                    console.log('New episode, getting data!');
+                    getEpisodeData( tvShow, function(episodedata) {
+                        var season = episodedata.tvShowSeason;
+                        var episode = episodedata.tvShowEpisode;
+                        var showTitle = episodedata.tvShowTitle;
+                        var showMetadata = [originalTitle, showTitle, season, episode ];
+
+                        storeEpisodeMetadataInDatabase(showMetadata, function() {
+                            console.log('Epsiode data stored in database...', showMetadata);
+                            var tvShowtitle = showMetadata.showTitle;
+                            loadShowMetadataFromDatabase(tvShowtitle, callback);
+                        });
+                    });
                 }
-            	 if (traktResult.certification !== undefined) {
-                	certification = traktResult.certification;
-                }
-				}
-				
-				var metadata = [originalTitle, tvTitle, banneImage, genre, certification ];
-				console.log('metadata',metadata);
-				storeMetadataInDatabase(metadata, function() {
-               console.log('data stored in database!');
-					loadMetadataFromDatabase(tvTitle, callback);
-				});
-			});
-		});
-	});
+
+            });
+
+        });
+    });
 };
 
 /* Private Methods */
 
-loadMetadataFromDatabase = function(tvTitle, callback) {
-	db.query('SELECT * FROM tvshows WHERE localName =? ', [ tvTitle ], {
+
+getMetadataForShow = function(tvTitle, callback){
+    var genre = "No data";
+    var certification = "No data";
+    var showTitle = 'Unknown show';
+
+    getMetadataFromTrakt(tvTitle, function(err, result){
+        if (err) {
+            console.error('Error returning Trakt data', err);
+        } else {
+            var traktResult = result;
+            var banner	= traktResult.images.banner;
+
+
+            showTitle = traktResult.title;
+            app_cache_handler.ensureCacheDirExists('tv', showTitle);
+
+            downloadTvShowBanner(banner, showTitle, function(err) {
+                if (err) {
+                    var bannerImage= "/tv/css/img/nodata.jpg";
+                } else {
+                    var bannerImage = '/data/tv/'+tvTitle+'/'+path.basename(banner);
+                }
+
+                if(traktResult !== null){
+                    if(traktResult.genres !== undefined){
+                        genre = traktResult.genre;
+                    }
+                    if (traktResult.certification !== undefined) {
+                        certification = traktResult.certification;
+                    }
+                    if(traktResult.title !== undefined){
+                        showTitle = traktResult.title;
+                    }
+                }
+
+                var showMetaData = [showTitle, bannerImage, genre, certification];
+                callback(showMetaData);
+
+            });
+
+        }
+
+    });
+}
+
+
+loadEpisodeMetadataFromDatabase = function(tvTitle, callback) {
+	db.query('SELECT * FROM tvepisodes WHERE localName =? ', [ tvTitle ], {
             localName 			: String,
             title 		    	: String,
-            banner  	    		: String,
-            genre  	        	: String,
-            certification  	: String
+            season  	        : String,
+            epsiode  	       	: String
 		},
 		function(rows) {
 			if (typeof rows !== 'undefined' && rows.length > 0){
@@ -99,12 +142,38 @@ loadMetadataFromDatabase = function(tvTitle, callback) {
 	);
 };
 
-storeMetadataInDatabase = function(metadata, callback) {
-	db.query('INSERT OR REPLACE INTO tvshows VALUES(?,?,?,?,?)', metadata);
+loadShowMetadataFromDatabase = function(tvTitle, callback) {
+    db.query('SELECT * FROM tvshows WHERE title =? ', [ tvTitle ], {
+            localName 			: String,
+            banner 		    	: String,
+            genre  	            : String,
+            certification  	    : String
+        },
+        function(rows) {
+            if (typeof rows !== 'undefined' && rows.length > 0){
+                console.log('found info for tvShow' .green);
+                console.log(rows)
+                callback(rows);
+            } else {
+                console.log('new tvshow' .green);
+                callback(null);
+            }
+        }
+    );
+};
+
+
+storeShowMetadataInDatabase = function(metadata, callback) {
+	db.query('INSERT OR REPLACE INTO tvshows VALUES(?,?,?,?)', metadata);
 	callback();
 };
 
-fetchMetadataFromTrakt = function(tvShow, callback) {
+storeEpisodeMetadataInDatabase = function(metadata, callback) {
+    db.query('INSERT OR REPLACE INTO tvepisodes VALUES(?,?,?,?)', metadata);
+    callback();
+};
+
+getMetadataFromTrakt = function(tvShow, callback) {
     var options = { query: tvShow }
     , Trakt = require('trakt')
     , trakt = new Trakt({username: 'mediacenterjs', password: 'mediacenterjs'});
@@ -127,15 +196,31 @@ downloadTvShowBanner = function(banner, tvShow, callback) {
 		try {
             app_cache_handler.downloadDataToCache('tv', tvShow, banner, function(err) {
                 if (err) {
+                    callback('Error downloading to cache ');
                     callback(err);
                 } else {
                     callback(null);
                 }
             });
 		} catch (exception) {
-			callback(exception);
+            console.log('Error downloading to cache ');
+            callback(exception);
 		}
 	} else {
 		callback('No banner specified!');
 	}
 };
+
+getEpisodeData = function(tvTitle, callback){
+    var tvshowdata = tvTitle.replace(/[sS]([0-9]{2})[eE]([0-9]{2})/, '')
+    , tvShowSeasonMatch = tvTitle.match(/[sS]([0-9]{2})/)
+    , tvShowEpisodeMatch = tvTitle.match(/[eE]([0-9]{2})/)
+    , season = tvShowSeasonMatch[0].replace(/[sS]/,"")
+    , episode = tvShowEpisodeMatch[0].replace(/[eE]/,"")
+    , episodeData = {
+        "tvShowTitle"      : tvshowdata,
+        "tvShowSeason"     : season,
+        "tvShowEpisode"    : episode
+    }
+    callback(episodeData);
+}
--- apps/tv/tv-functions.js
+++ apps/tv/tv-functions.js
@@ -1,9 +1,9 @@
 /* Global Imports */
 var fs = require('fs.extra')
 	, file_utils = require('../../lib/utils/file-utils')
-	, app_cache_handler = require('../../lib/handlers/app-cache-handler')
 	, colors = require('colors')
 	, os = require('os')
+    , metadata_fetcher = require('./metadata-fetcher')
 	, config = require('../../lib/handlers/configuration-handler').getConfiguration();
 
 /* Constants */
@@ -18,14 +18,12 @@ db.on('info', function (text) { console.log(text) });
 db.on('error', function (err) { console.error('Database error: ' + err) });
 
 //Create tables
-db.query("CREATE TABLE IF NOT EXISTS tvshows (localName TEXT PRIMARY KEY,title VARCHAR,banner VARCHAR, genre VARCHAR, certification VARCHAR)");
+db.query("CREATE TABLE IF NOT EXISTS tvepisodes (localName TEXT PRIMARY KEY,title VARCHAR, season VARCHAR, epsiode VARCHAR)");
+db.query("CREATE TABLE IF NOT EXISTS tvshows (title TEXT PRIMARY KEY,banner VARCHAR, genre VARCHAR, certification VARCHAR)");
 db.query("CREATE TABLE IF NOT EXISTS progressionmarker (title TEXT PRIMARY KEY, progression TEXT, transcodingstatus TEXT)");
 
 exports.loadItems = function (req, res){
 	file_utils.getLocalFiles(config.tvpath, SUPPORTED_FILETYPES, function(err, files) {
-
-        console.log('files', files);
-
 		var tvShows = [];
 		for(var i = 0, l = files.length; i < l; ++i){
 			var tvShowFiles = files[i].file;
@@ -43,32 +41,27 @@ exports.loadItems = function (req, res){
 	});
 };
 
-exports.playtvShow = function (req, res, platform, tvShowRequest){
- 
-	file_utils.getLocalFile(config.tvpath, tvShowRequest, function(err, file) {
-		if (err) console.log(err .red);
-		if (file) {
-			var tvShowUrl = file.href
-			, tvShow_playback_handler = require('./tv-playback-handler');
-			
-			tvShow_playback_handler.startPlayback(res, tvShowUrl, tvShowRequest, platform);
-    
-		} else {
-			console.log("File " + tvShowRequest + " could not be found!" .red);
-		}
-	});
-
-};
-
 exports.handler = function (req, res, tvShowRequest){
-	//Modules
-	var downloader = require('downloader');
-
 	console.log('Searching for ' + tvShowRequest + ' in database');
-    var metadata_fetcher = require('./metadata-fetcher');
+
 	metadata_fetcher.fetchMetadataForTvShow(tvShowRequest, function(metadata) {
 		res.json(metadata);
 	});
+};
+
+exports.playtvShow = function (req, res, platform, tvShowRequest){
+    file_utils.getLocalFile(config.tvpath, tvShowRequest, function(err, file) {
+        if (err) console.log(err .red);
+        if (file) {
+            var tvShowUrl = file.href
+            , tvShow_playback_handler = require('./tv-playback-handler');
+
+            tvShow_playback_handler.startPlayback(res, tvShowUrl, tvShowRequest, platform);
+
+        } else {
+            console.log("File " + tvShowRequest + " could not be found!" .red);
+        }
+    });
 };
 
 exports.sendState = function (req, res){
