eb923850b54a067c59919907f0395d61795e62ac
Robin Larsson <tankmaster93@gmail.com>
2014-03-03 21:21:12 +0100

    Added Swedish translation

--- public/translations/frontend/translation_sv.js
+++ public/translations/frontend/translation_sv.js
@@ -3,4 +3,4 @@ weather_current = Vädret just nu
 weather_underground_languagecode = SN
 feelsLike = Känns som
 error_weather =  Specificera en större stad.
-confirmMessage = Är du säker på att du vill göra detta? Denna handling kan inte ogöras.
+confirmMessage = Är du säker på att du vill göra detta? Denna handling kan inte göras om.
