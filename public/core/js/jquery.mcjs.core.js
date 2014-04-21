e8d9f370619d7896957fad58f592f6c3e7187b87
jansmolders86 <jansmolders86@gmail.com>
2014-04-21 15:01:37 +0200

    Added preambles

--- public/core/js/jquery.mcjs.core.js
+++ public/core/js/jquery.mcjs.core.js
@@ -88,7 +88,7 @@
 	
 	function _initpages(o, $that){
 		
-		// Setup frontend validation
+		// Setup frontend translations
 		$.ajax({
 			url: '/configuration/', 
 			type: 'get'
--- public/music/js/remote.js
+++ public/music/js/remote.js
@@ -1,3 +1,20 @@
+/*
+	MediaCenterJS - A NodeJS based mediacenter solution
+	
+    Copyright (C) 2014 - Jan Smolders
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 function remote(socket, $scope, player, audio){
     var albums = $scope.albums;
     socket.on('controlling', function(data){
