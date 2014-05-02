169ff16799a77fcc4b1122e54ba784d1ce49bf22
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2014-05-02 19:13:46 +0200

    Italian translation

---
+++ public/translations/frontend/translation_it.js
@@ -0,0 +1,6 @@
+succesMessage = Eseguito Correttamente
+weather_current = Condizioni correnti
+weather_underground_languagecode = IT
+feelsLike = Sentirsi come
+error_weather = Specifica una grande città.
+confirmMessage = Sei sicuro di ciò che fai? Questa azione non potrà essere annullata.
