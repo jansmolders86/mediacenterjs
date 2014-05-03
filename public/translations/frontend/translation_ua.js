25d6975a6772d17c52122147f86347e9207447b7
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2014-05-04 01:28:38 +0300

    Russian and ukrainian translations

---
+++ public/translations/frontend/translation_ua.js
@@ -0,0 +1,6 @@
+succesMessage = Успішно виконано.
+weather_current = Поточні умови
+weather_underground_languagecode = UA
+feelsLike = Відчувається як
+error_weather =  Будь ласка, вкажіть більший город.
+confirmMessage = Ви впевнені? Ця дія буде безповоротною.
