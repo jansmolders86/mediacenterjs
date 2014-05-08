4ef779075ee5eba5c72b8fef3105d39237bb2ef9
Alberto Jerez <albertoj04@gmail.com>
2014-05-08 23:51:13 +0200

    Added Spanish translation

---
+++ public/translations/frontend/translation_es.js
@@ -0,0 +1,6 @@
+succesMessage = Ejecutado con éxito.
+weather_current = Condiciones actuales
+weather_underground_languagecode = ES
+feelsLike = Sensación térmica
+error_weather =  Por favor, indica una ciudad más grande.
+confirmMessage = ¿Estás seguro de que quieres hacer ésto? Esta acción no se puede deshacer.
