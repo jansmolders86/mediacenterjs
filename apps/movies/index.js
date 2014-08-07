5a7abea6b6a59882775de92a3b60823ff52a927f
Jonathan Bailey <jonathan@dyn1205-192.wlan.ic.ac.uk>
2014-08-08 00:13:03 +0100

    Fix merge issue in movies/index.js

--- apps/movies/index.js
+++ apps/movies/index.js
@@ -91,10 +91,9 @@ exports.post = function(req, res, next){
     }
     else if(req.params.id === 'edit'){
         functions.edit(req, res, data);
+    } else if(req.params.id === 'update'){
+        functions.update(req, res, data);
     } else {
         next();
-    }
-    else if(req.params.id === 'update'){
-        functions.update(req, res, data);
     }
 }
