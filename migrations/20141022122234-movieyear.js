370edd86f3459008db1044971346ade9b0daa5ef
Andre Kanig <akanig@datameer.com>
2014-11-12 12:04:01 +0100

    added check for existing column

--- migrations/20141022122234-movieyear.js
+++ migrations/20141022122234-movieyear.js
@@ -1,10 +1,28 @@
 module.exports = {
   up: function(migration, DataTypes, done) {
-  	migration.addColumn('Movies', 'year', DataTypes.INTEGER);
-    done()
+    migration.showAllTables().success(function(tables) {
+      if (tables.indexOf('Movies') === -1) {
+        return done();
+      }
+
+      migration.describeTable('Movies').success(function(table) {
+        if (table.year) return done();
+        migration.addColumn('Movies', 'year', DataTypes.INTEGER);
+        done();
+      }).error(done);
+    }).error(done);
   },
   down: function(migration, DataTypes, done) {
-  	migration.removeColumn('Movies', 'year');
-    done()
+    migration.showAllTables().success(function(tables) {
+      if (tables.indexOf('Movies') === -1) {
+        return done();
+      }
+
+      migration.describeTable('Movies').success(function(table) {
+        if (!table.year) return done();
+        migration.removeColumn('Movies', 'year');
+        done();
+      }).error(done);
+    }).error(done);
   }
-}
+};
