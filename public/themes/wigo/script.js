a6a4e766dae0ed8ec83de75abf1f099981f38ef0
Jan Smolders <jansmolders86@gmail.com>
2014-11-14 11:22:47 +0100

    Added fun/simple app loading animation on homepage

--- public/themes/wigo/script.js
+++ public/themes/wigo/script.js
@@ -1,9 +1,21 @@
 $(function() {
 
 
+
+
     if($('body').hasClass('homepage')){
 
-        $('.tile[class*=mediacenterjs-]').addClass('plugin')
+        $('.tile[class*=mediacenterjs-]').addClass('plugin');
+
+        $('.tile a').on('click', function(e){
+            e.preventDefault();
+            var that = $(this);
+            that.parent().addClass('grow');
+            setTimeout(function(){
+                var url = that.attr('href');
+                window.location.href = url;
+            },100);
+        });
 
         var index = 0;
         var length = $("ul").children().length;
