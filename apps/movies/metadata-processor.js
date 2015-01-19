ca28f6e60e356052cac616f8f88560982217fa98
Stefan Hoffmann <sthoffmann93@googlemail.com>
2015-01-19 21:57:35 +0100

    Add Movies and Shows also when no informations were found on TMDB #180

--- apps/movies/metadata-processor.js
+++ apps/movies/metadata-processor.js
@@ -14,43 +14,58 @@ exports.processFile = function (fileObject, callback) {
                           language: config.language,
                           year: movieInfo.year };
 
+    var metadata = {
+        filePath        : path.normalize(fileObject.href),
+        title           : movieTitle,
+        posterURL       : '/movies/css/img/nodata.jpg',
+        backgroundURL   : '/movies/css/img/backdrop.jpg',
+        imdbID          : 0,
+        rating          : 'Unknown',
+        genre           : 'Unknown',
+        runtime         : 'Unknown',
+        overview        : '',
+        adult           : false,
+        hidden          : 'false',
+        year            : movieInfo.year
+    };
+
     moviedb.searchMovie(searchOptions, function(err, result) {
         if (err || (result && result.results.length < 1)) {
             console.log('Error retrieving data for ' + movieTitle, err);
-            callback();
         } else {
             result = result.results[0];
-            if (!result) callback();
 
-            var baseUrl = 'http://image.tmdb.org'
-                , posterURL = result.poster_path ? baseUrl + '/t/p/w342' + result.poster_path : '/movies/css/img/nodata.jpg'
-                , backgroundURL = result.backdrop_path ? baseUrl + '/t/p/w1920' + result.backdrop_path : '/movies/css/img/backdrop.jpg';
+            var posterURL = buildImageUrl('342', result.poster_path) || metadata.posterURL;
+            var backgroundURL = buildImageUrl('1920', result.backdrop_path) || metadata.backgroundURL;
 
-            var metadata = {
-                filePath        : path.normalize(fileObject.href),
-                title           : result.title ? result.title : movieTitle,
-                posterURL       : posterURL,
-                backgroundURL   : backgroundURL,
-                imdbID          : result.imdb_id,
-                rating          : result.vote_average ? result.vote_average.toString() : 'Unknown',
-                genre           : result.genres && result.genres.length ? result.genres[0].name : 'Unknown',
-                runtime         : result.runtime ? result.runtime : 'Unknown',
-                overview        : result.overview,
-                adult           : result.adult ? result.adult : false,
-                hidden          : "false",
-                year            : result.release_date ? new Date(result.release_date).getFullYear() : movieInfo.year
-            };
-
-            Movie.find({ filePath: metadata.filePath }).complete(function (err, movie) {
-                if (err || !movie) {
-                    Movie.create(metadata).success(function(err, movie) {
-                        callback();
-                    });
-                } else {
-                    movie.updateAttributes(metadata);
-                    callback();
-                }
-            });
+            metadata.title = result.title;
+            metadata.posterURL = posterURL;
+            metadata.backgroundURL = backgroundURL;
+            metadata.imdbID = result.imdbID;
+            metadata.rating = result.vote_average.toString();
+            metadata.genre = result.genres && result.genres.length ? result.genres[0].name : metadata.genre;
+            metadata.runtime = result.runtime || 'Unknown';
+            metadata.overview = result.overview;
+            metadata.adult = result.adult === 'true';
+            metadata.year = result.release_date ? new Date(result.release_date).getFullYear() : metadata.year;
         }
+
+        Movie.find({ where: { filePath: metadata.filePath } }).complete(function (err, movie) {
+            console.log(err, movie, metadata.filePath);
+            if (err || !movie) {
+                Movie.create(metadata).success(function(err, movie) {
+                    callback();
+                });
+            } else {
+                movie.updateAttributes(metadata);
+                callback();
+            }
+        });
     });
 }
+
+function buildImageUrl(width, path) {
+  if (!path) return;
+  return "http://image.tmdb.org/t/p/w" + width + path;
+}
+
