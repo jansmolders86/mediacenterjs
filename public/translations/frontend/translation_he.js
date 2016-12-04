124b3b9589b8a8528fd953fb95f957e692e8244e
sniraga <sniraga@gmail.com>
2016-12-04 05:25:08 +0200

    Create translation_he.js

---
+++ public/translations/frontend/translation_he.js
@@ -0,0 +1,6 @@
+succesMessage = בוצע בהצלחה
+weather_current = תנאים נוכחיים
+weather_underground_languagecode = HE
+feelsLike = מרגיש כמו
+error_weather =  בבקשה ציין עיר גדולה
+confirmMessage = האם אתה בטוח ? פעולה זו אינה הפיכה
