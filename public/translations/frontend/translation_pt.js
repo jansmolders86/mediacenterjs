fc84c8459f8bc90478b8f961e3717c0f1719e22f
Leo Caseiro <leobok@gmail.com>
2017-02-28 00:58:22 +1100

    Update PT Trasnlations

--- public/translations/frontend/translation_pt.js
+++ public/translations/frontend/translation_pt.js
@@ -1,6 +1,6 @@
 succesMessage = Executado com sucesso.
-weather_current = condições atuais
+weather_current = Condições atuais
 weather_underground_languagecode = BR
-feelsLike = Feels like
-error_weather =  Especifique uma cidade maior
+feelsLike = Aparenta
+error_weather =  Especifique uma cidade maior.
 confirmMessage = Tem certeza que deseja fazer isso? Essa ação não poderá ser revertida.
